d8a9e555f6e59e088423cc7df8b15cf00858770f
vikas ghodke <vikas ghodke@DESKTOP-U4L7FDE>
2022-09-26 20:48:35 +0530

    First update on x + y project

---
+++ Day_1_First_Day/src/com/vikas/basic/first_project.java
@@ -0,0 +1,16 @@
+package com.vikas.basic;
+
+public class first_project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10, y=10;
+		int c = x + y;
+		
+		
+		
+		System.out.println(c);
+
+	}
+
+}
