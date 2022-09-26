d51c5723ef516fbf0a57c41f57b78d273a382e28
vikas ghodke <vikas ghodke@DESKTOP-U4L7FDE>
2022-09-26 20:58:32 +0530

    second-program

---
+++ first_javaproject/src/com/vikas/demo/Fisrt_project.java
@@ -0,0 +1,18 @@
+package com.vikas.demo;
+
+public class Fisrt_project {
+
+	public static void main(String[] args) {
+	
+		
+		 
+		
+		
+		
+		int a=1, b=2, c = a+b;
+		System.out.println(c);
+		
+
+	}
+
+}
