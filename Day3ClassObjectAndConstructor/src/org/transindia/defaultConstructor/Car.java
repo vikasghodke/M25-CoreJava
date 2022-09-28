618d49ac19a5c75c09ce8435fb9b73069224704b
vikas ghodke <vikas ghodke@DESKTOP-U4L7FDE>
2022-09-28 18:33:00 +0530

    day3

---
+++ Day3ClassObjectAndConstructor/src/org/transindia/defaultConstructor/Car.java
@@ -0,0 +1,20 @@
+package org.transindia.defaultConstructor;
+
+import java.util.Scanner;
+
+public class Car {
+
+	public static void main(String[] args) {
+		
+		Scanner in=new Scanner(System.in);
+		
+		System.out.println("Enter a value of x=");
+		//char x=in.next().charAt(4);
+		
+		System.out.println("result in ");
+		System.out.println(x);
+		
+		
+
+}
+}
