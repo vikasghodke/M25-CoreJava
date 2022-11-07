cb0d1b9425654b62f69fc4dfb6ce5dfea14564d9
vikas ghodke <vikas ghodke@DESKTOP-U4L7FDE>
2022-11-07 17:13:40 +0530

    first commit

---
+++ Day-17_Annotation/src/org/tnsindia/annotation/Example3.java
@@ -0,0 +1,26 @@
+package org.tnsindia.annotation;
+
+class Parent1
+{
+	@Deprecated
+	void print()
+	{
+		System.out.println("Hey guys!");
+	}
+	@Deprecated
+	void print1()
+	{
+		System.out.println("Hurrah!");
+	}
+}
+
+
+public class Example3 {
+	
+	public static void main(String[] args) {
+		Parent1 p=new Parent1();
+		p.print1();
+	}
+
+
+}
---
+++ Day-17_Annotation/src/org/tnsindia/annotation/Parent.java
@@ -0,0 +1,31 @@
+package org.tnsindia.annotation;
+
+public class Parent {
+	
+	
+	
+	void eatFood()
+	{
+		System.out.println("Parent class");
+	}
+}
+class Child extends Parent
+{
+	@Override
+	void eatFood()
+	{
+		System.out.println("Child class");
+	}
+}
+public class Example1 {
+
+	public static void main(String[] args) {
+		Parent c=new Child();
+		c.eatFood();
+
+
+	
+
+	}
+
+}
