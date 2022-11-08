fb47ec51e81789a2b453a7a6d28440956589eb44
vikas ghodke <vikas ghodke@DESKTOP-U4L7FDE>
2022-11-08 22:56:23 +0530

    first commit

---
+++ Day-21_ConcurrentPattern/src/org/tnsindia/concurancy/MultiplyingTask.java
@@ -0,0 +1,26 @@
+package org.tnsindia.concurancy;
+
+import java.util.concurrent.Callable;
+
+public class MultiplyingTask implements Callable {
+	
+	int x,y;
+	long sleeptime;
+	
+
+	//constructor
+	public MultiplyingTask(int x, int y, long sleeptime) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.sleeptime = sleeptime;
+	}
+
+	@Override
+	public Object call() throws Exception {
+		Thread.sleep(sleeptime);
+		return x*y;
+	}
+}
+
+
